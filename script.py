8df6b1f8b31df66ad87c9d7e49d0fda9a1f9a3a6
M23CSA009 <m23csa009@iitj.ac.in>
2024-04-04 16:21:35 +0530

    modified existing file

--- script.py
+++ script.py
@@ -15,7 +15,8 @@ def tanh(x):
     return np.tanh(x)
 
 #random values for plotting graph
-x = np.linspace(-5, 5, 100)
+x = np.array([-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6])
+x.sort()
 
 y_sigmoid = sigmoid(x)
 y_relu = relu(x)
